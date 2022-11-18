e60e9d3baf4eb6165c62720a29fe224540ee87f7
MaxJackson13 <72030655+MaxJackson13@users.noreply.github.com>
2022-11-18 15:43:58 +0000

    Update and rename Execute-AsAnother.ps1 to Invoke-Execution.ps1

--- Invoke-Execution.ps1
+++ Invoke-Execution.ps1
@@ -1,12 +1,12 @@
 '''
 Quickly execute powershell commands under the context of another user, given their username and password.
-Run '. .\Execute-AsAnother.ps1' from the directory in which the ps1 file is located.
+Run '. .\Invoke-Execution.ps1' from the directory in which the ps1 file is located.
 '''
 
-function Execute-AsAnother {
+function Invoke-Execution {
         params($Username, $Password, $Command)
         $SecurePassword = ConvertTo-SecureString $Password -AsPlaintext -Force
-        $Credential = New-Object System.Management.Automation.PSCredential $Username,$SecurePassword
+        $Credential = New-Object System.Management.Automation.PSCredential($Username,$SecurePassword)
         $Computer = hostname
         $Command = Invoke-Command -ComputerName $Computer -Credential $Credential -ScriptBlock { $Command }
         Write-Host $Command
