961b2b1311f15741a764f898123c19e01893bf21
MaxJackson13 <72030655+MaxJackson13@users.noreply.github.com>
2022-11-18 20:23:14 +0000

    Update Invoke-Execution.ps1

--- Invoke-Execution.ps1
+++ Invoke-Execution.ps1
@@ -1,9 +1,9 @@
-'''
-Quickly execute powershell commands under the context of another user, given their username and password.
-Run '. .\Invoke-Execution.ps1' from the directory in which the ps1 file is located.
-'''
-
 function Invoke-Execution {
+        #.SYNOPSIS
+        # Execute powershell commands under the context of another user, given their username and password.
+        # Run '. .\Invoke-Execution.ps1' from the directory in which the ps1 file is located.
+        ###################################################################################################
+        
         params($Username, $Password, $Command)
         $SecurePassword = ConvertTo-SecureString $Password -AsPlaintext -Force
         $Credential = New-Object System.Management.Automation.PSCredential($Username,$SecurePassword)
