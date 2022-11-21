95274adfbf8811ed8c89474a3c94f25abdcce043
MaxJackson13 <72030655+MaxJackson13@users.noreply.github.com>
2022-11-21 22:03:02 +0000

    Update DetectPasswordSpray.ps1

--- DetectPasswordSpray.ps1
+++ DetectPasswordSpray.ps1
@@ -1,3 +1,40 @@
+#define password spray as more than 3 or more failed logins to different users from the same ip in short time (30 min)
+
+$r=(get-winevent -filterhashtable @{logname='security';id=4771} -maxevents 100)
+$obj=@()
+foreach ($record in $r) {
+    [xml]$evt=$record.toxml()
+    $data=$evt.event.eventdata.data
+    $obj+=[PSCustomObject] @{
+        'TargetUserName' = $data[0].'#text'
+        'Status' = $data[4].'#text'
+        'SourceIp' = $data[6].'#text'
+        'TimeCreated' = $record.timecreated.datetime
+    }
+}
+$obj
+
+-group by ip
+-define start as most recent timecreated
+-check next, if different user and time within 30 mins of previous, move to next
+-otherwise update start
+
+$g=$groups[$i].group
+$start=[DateTime]($g[0].timecreated)
+$j=1
+while ($j -lt $g.count){
+  if (($start-[DateTime]($g[$j].timecreated)).seconds -lt 1800)
+  {$j+=1}
+  else {
+  $start=[DateTime]($g[$j].timecreated
+  
+  }
+  
+}
+
+
+
+
 eventid 4678 A Kerberos authentication ticket (TGT) was requested
 result code 0x6	KDC_ERR_C_PRINCIPAL_UNKNOWN if invalid user
 
