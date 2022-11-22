09e1db364162bac10b6e2b05d03f9f59836cf6e7
MaxJackson13 <72030655+MaxJackson13@users.noreply.github.com>
2022-11-22 11:53:40 +0000

    Update DetectPasswordSpray.ps1

--- DetectPasswordSpray.ps1
+++ DetectPasswordSpray.ps1
@@ -1,72 +1,36 @@
-#define password spray as more than 3 or more failed logins to different users from the same ip in short time (30 min)
+# The Windows security event log ID 4771 corresponds to a Kerberos preauthentication failure. Failure code 0x18 means the pre-authentication information   # provided was invalid, the most likely cause for this being a wrong password was supplied. In a password spray attackers will try common passwords, or     # passwords they've found which they know are used in the domain (e.g. default logon passwords) against many valid users accounts. Therefore in the logs   # there'll be many authentication failures against different accounts originating from a single source IP.
 
-$r=(get-winevent -filterhashtable @{logname='security';id=4771} -maxevents 100)
-$obj=@()
-foreach ($record in $r) {
+$logRecords=(Get-Winevent -FilterHashtable @{ LogName='security'; id=4771 } -MaxEvents 100)
+
+$object=@()
+
+foreach ($record in $logRecords) {
+    
     [xml]$evt=$record.toxml()
+    
     $data=$evt.event.eventdata.data
-    $obj+=[PSCustomObject] @{
+    
+    $object+=[PSCustomObject] @{
         'TargetUserName' = $data[0].'#text'
         'Status' = $data[4].'#text'
         'SourceIp' = $data[6].'#text'
         'TimeCreated' = $record.timecreated.datetime
     }
 }
-$obj
 
--group by ip
--define start as most recent timecreated
--check next, if different user and time within 30 mins of previous, move to next
--otherwise update start
+$groups=($object | Group-Object 'sourceip')
 
-$g=$groups[$i].group
-$start=[DateTime]($g[0].timecreated)
-$j=1
-while ($j -lt $g.count){
-  if (($start-[DateTime]($g[$j].timecreated)).seconds -lt 1800)
-  {$j+=1}
-  else {
-  $start=[DateTime]($g[$j].timecreated
+for ($i=0; $i -lt $groups.count; $i++){
   
-  }
+  $group=$groups[$i].group
   
+  $length=$group.count
+  
+  Write-Host $group
+ 
+  #$start=[DateTime]($g[0].timecreated)
+  #$end=[DateTime]($g[$j-1].timecreated)
+  #echo ($start-$end).totalseconds
+  echo ''
 }
-
-
-
-
-eventid 4678 A Kerberos authentication ticket (TGT) was requested
-result code 0x6	KDC_ERR_C_PRINCIPAL_UNKNOWN if invalid user
-
-eventid 4771 Kerberos pre-authentication failed if valid user invalid pass
-
-
-get-aduser -filter * -searchbase 'DC=SCRM,DC=LOCAL' | select samaccountname 2>&1
-
-samaccountname
---------------
-administrator 
-Guest         
-krbtgt        
-tstar         
-asmith        
-sjenkins      
-sdonington    
-backupsvc     
-jhall         
-rsmith        
-ehooker       
-khicks        
-sqlsvc        
-miscsvc       
-ksimpson      
-
-Get-ADRootDSE | select *naming* 
-
-configurationNamingContext : CN=Configuration,DC=scrm,DC=local
-defaultNamingContext       : DC=scrm,DC=local
-namingContexts             : {DC=scrm,DC=local, CN=Configuration,DC=scrm,DC=local, 
-                             CN=Schema,CN=Configuration,DC=scrm,DC=local, DC=DomainDnsZones,DC=scrm,DC=local...}
-rootDomainNamingContext    : DC=scrm,DC=local
-schemaNamingContext        : CN=Schema,CN=Configuration,DC=scrm,DC=local
 
