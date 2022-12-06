7fc7bcc8d4d18af5e1f32b94ab761335fd87665b
MaxJackson13 <72030655+MaxJackson13@users.noreply.github.com>
2022-12-06 15:23:15 +0000

    Create PsExecDetect.ps1

---
+++ PsExecDetect.ps1
@@ -0,0 +1,38 @@
+Function Find-PsExec {
+Function Find-PsExec {
+    [cmdletbinding()]
+    Param (
+        [parameter()]
+        [switch]$GetLogs
+    )
+    
+    $Executables = Get-ChildItem -Path $env:SYSTEMROOT *.exe
+
+    $Unsigned = $Executables | ForEach-Object { if ((Get-AuthenticodeSignature $_.FullName).Status -eq 'notsigned') {$_}}
+
+    $AllServices = (Get-Service).Name
+
+    $RegEntry = $AllServices | ForEach-Object {Get-ItemProperty -ErrorAction Ignore "HKLM:\SYSTEM\CurrentControlSet\Services\$($_)"}
+
+    $Services = $RegEntry | Where-Object {$_.ImagePath -match "$([Regex]::Escape($env:SYSTEMROOT))\\\w+\.exe$"}
+
+    ForEach ($Exe in $Unsigned) {
+        if($Exe.FullName -in $Services.ImagePath) {
+            $Service = $Services | Where-Object {$Exe.FullName -eq $_.ImagePath}
+            Write-Host "`n$($Exe.FullName) is an executable for the service $($Service.DisplayName)`n"
+            ($Service | Out-String).Trim()
+
+        }
+    }
+    
+    if ($GetLogs) {
+        ForEach ($Exe in $Unsigned) {
+            if($Exe.FullName -in $Services.ImagePath) {
+                $CreatedAt = $Exe.CreationTime
+                $Before = $CreatedAt.AddSeconds(5)
+                $After = $CreatedAt.AddSeconds(-5)
+                $Logs = Get-EventLog -Logname Security -After $After -Before $Before -ErrorAction Ignore
+                
+    }
+
+}
